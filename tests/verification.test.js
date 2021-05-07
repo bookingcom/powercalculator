af993733519a4e96ebcc04c9762329eb603f0fb0
Alberto Martinez De Murga Ramirez <alberto.demurga@booking.com>
2021-05-07 16:41:03 +0200

    tests: Add verification tests for non-inferiority

--- tests/verification.test.js
+++ tests/verification.test.js
@@ -8,7 +8,7 @@ import {
   COMPARISON_MODE,
   TRAFFIC_MODE,
   TEST_TYPE,
-  VALUE_TYPE,
+  VALUE_TYPE
 } from '../src/store/modules/calculator'
 
 const { tTest, getRelativeImpactFromAbsolute, getCorrectedAlpha } = math
@@ -25,142 +25,285 @@ const { power, sample, impact } = tTest
 // error message in the expect and match the output with a regexp in order to
 // have visibility on the errors.
 const TOLERANCE = 0.0005
+const DATASET_SIZE = 9234
 const similar = (val, apr) => Math.abs(val - apr) / val
 const executeTest = (idx, val, apr) => {
   const err = similar(val, apr)
-  return `idx=${idx},out=${err < TOLERANCE},val=${val},apr=${apr},err=${err}`
+  const out = `idx=${idx},out=${err < TOLERANCE},val=${val},apr=${apr},err=${err}`
+  return out
 }
 const skipTest = idx => `idx=${idx},out=skipped`
 
-const cases = []
+const entries = []
 beforeAll(() => {
   try {
-    const content = readFileSync(resolve(join(__dirname, './comparative.dataset.csv')), {
+    const content = readFileSync(resolve(join(__dirname, './dataset.csv')), {
       encoding: 'utf-8',
-      flag: 'r',
+      flag: 'r'
     })
-    const csv = parse(content, { columns: true })
-    for (const record of csv) {
-      cases.push({
-        baseRate: +record.point_estimate,
-        confidenceLevel: 0.9,
-        falsePositiveRate: 0.1,
-        sample: +record.sample_size,
-        targetPower: +record.power,
-        // runtime,
-        // visitorsPerDay,
-        standardDeviation: +record.stddev_base,
-        variants: record.nr_variants - 1,
-        impact:
-          record.mre_type === VALUE_TYPE.ABSOLUTE
-            ? getRelativeImpactFromAbsolute({
-                base_rate: +record.point_estimate,
-                absolute_effect_size: +record.effect_size,
-              })
-            : +record.effect_size,
-
-        // Configuration
-        isNonInferiority: false,
-        comparisonMode: COMPARISON_MODE.ALL,
-        trafficMode: TRAFFIC_MODE.TOTAL,
-        testType: TEST_TYPE.CONTINUOUS,
-      })
-    }
+    entries.push(...parse(content, { columns: true }))
   } catch (e) {
     console.error(`Error parsing the dataset: ${e}`)
   }
 })
 
 describe('Verifying math library', () => {
-  test('Calculating power', () => {
-    const results = cases.map((entry, index) => {
-      const {
-        targetPower,
-        sample,
-        falsePositiveRate,
-        standardDeviation,
-        baseRate,
-        impact,
-        alpha,
-        variants,
-      } = entry
-      const approx = power({
-        total_sample_size: sample,
-        base_rate: baseRate,
-        effect_size: impact,
-        alpha: getCorrectedAlpha(falsePositiveRate, variants),
-        variants,
-        sd_rate: standardDeviation,
-        alternative: 'two-sided',
-        mu: 0,
-      })
-      return executeTest(index, targetPower, approx)
-    })
-
-    const errors = results.filter((r) => /false/.test(r))
-
-    expect(errors.length).toBe(0)
+  test('Validate dataset', () => {
+    expect(entries.length).toBe(DATASET_SIZE)
   })
+  describe('Comparative tests', () => {
+    const cases = []
+    beforeAll(() => {
+      for (const entry of entries) {
+        cases.push({
+          baseRate: +entry.point_estimate,
+          confidenceLevel: 0.9,
+          falsePositiveRate: 0.1,
+          sample: +entry.sample_size,
+          targetPower: +entry.power,
+          runtime: 10,
+          visitorsPerDay: +entry.sample_size / 10,
+          standardDeviation: +entry.stddev_base,
+          variants: entry.nr_variants - 1,
+          impact:
+            entry.mre_type === VALUE_TYPE.ABSOLUTE
+              ? getRelativeImpactFromAbsolute({
+                  base_rate: +entry.point_estimate,
+                  absolute_effect_size: +entry.effect_size
+                })
+              : +entry.effect_size,
 
-  test('Calculating sample', () => {
-    const results = cases.map((entry, index) => {
-      const {
-        targetPower,
-        sample: targetSample,
-        falsePositiveRate,
-        standardDeviation,
-        baseRate,
-        impact,
-        alpha,
-        variants,
-      } = entry
-
-      if (targetPower < 0.7 || targetPower >= 0.999) return skipTest(index)
-      const approx = sample({
-        base_rate: baseRate,
-        effect_size: impact,
-        alpha: getCorrectedAlpha(falsePositiveRate, variants),
-        beta: 1 - targetPower,
-        variants,
-        sd_rate: standardDeviation,
-        alternative: 'two-sided',
-        mu: 0,
+          // Configuration
+          isNonInferiority: false,
+          comparisonMode: COMPARISON_MODE.ALL,
+          trafficMode: TRAFFIC_MODE.TOTAL,
+          testType: TEST_TYPE.CONTINUOUS
+        })
+      }
+    })
+    test('Calculating power', () => {
+      const results = cases.map((entry, index) => {
+        const {
+          targetPower,
+          sample,
+          falsePositiveRate,
+          standardDeviation,
+          baseRate,
+          impact,
+          alpha,
+          variants
+        } = entry
+        const approx = power({
+          total_sample_size: sample,
+          base_rate: baseRate,
+          effect_size: impact,
+          alpha: getCorrectedAlpha(falsePositiveRate, variants),
+          variants,
+          sd_rate: standardDeviation,
+          alternative: 'two-sided',
+          mu: 0
+        })
+        return executeTest(index, targetPower, approx)
       })
-      return executeTest(index, targetSample, approx)
+
+      const errors = results.filter(r => /false/.test(r))
+
+      expect(errors.length).toBe(0)
     })
 
-    const errors = results.filter((r) => /false/.test(r))
-    expect(errors.length).toBe(0)
+    test('Calculating sample', () => {
+      const results = cases.map((entry, index) => {
+        const {
+          targetPower,
+          sample: targetSample,
+          falsePositiveRate,
+          standardDeviation,
+          baseRate,
+          impact,
+          alpha,
+          variants
+        } = entry
+
+        if (targetPower < 0.7 || targetPower >= 0.999) return skipTest(index)
+        const approx = sample({
+          base_rate: baseRate,
+          effect_size: impact,
+          alpha: getCorrectedAlpha(falsePositiveRate, variants),
+          beta: 1 - targetPower,
+          variants,
+          sd_rate: standardDeviation,
+          alternative: 'two-sided',
+          mu: 0
+        })
+        return executeTest(index, targetSample, approx)
+      })
+
+      const errors = results.filter(r => /false/.test(r))
+      expect(errors.length).toBe(0)
+    })
+
+    test('Calculating effect size', () => {
+      const results = cases.map((entry, index) => {
+        const {
+          targetPower,
+          sample,
+          falsePositiveRate,
+          standardDeviation,
+          baseRate,
+          impact: targetImpact,
+          alpha,
+          variants
+        } = entry
+        if (targetPower < 0.7 || targetPower >= 0.999) return skipTest(index)
+        const approx = impact({
+          total_sample_size: sample,
+          base_rate: baseRate,
+          alpha: getCorrectedAlpha(falsePositiveRate, variants),
+          beta: 1 - targetPower,
+          variants,
+          sd_rate: standardDeviation,
+          alternative: 'two-sided',
+          mu: 0
+        })
+        return executeTest(index, targetImpact, approx)
+      })
+
+      const errors = results.filter(r => /false/.test(r))
+      expect(errors.length).toBe(0)
+    })
   })
+  describe('Non-inferiority tests', () => {
+    const cases = []
+    beforeAll(() => {
+      for (const entry of entries) {
+        const opts = {
+          type: 'relative',
+          alternative: 'greater',
+          base_rate: +entry.point_estimate,
+          threshold: -entry.effect_size
+        }
 
-  test('Calculating effect size', () => {
-       const results = cases.map((entry, index) => {
-      const {
-        targetPower,
-        sample,
-        falsePositiveRate,
-        standardDeviation,
-        baseRate,
-        impact: targetImpact,
-        alpha,
-        variants,
-      } = entry
-      if (targetPower < 0.7 || targetPower >= 0.999) return skipTest(index)
-      const approx = impact({
-        total_sample_size: sample,
-        base_rate: baseRate,
-        alpha: getCorrectedAlpha(falsePositiveRate, variants),
-        beta: 1 - targetPower,
-        variants,
-        sd_rate: standardDeviation,
-        alternative: 'two-sided',
-        mu: 0,
+        cases.push({
+          baseRate: +entry.point_estimate,
+          confidenceLevel: 0.9,
+          falsePositiveRate: 0.1,
+          sample: +entry.sample_size,
+          targetPower: +entry.power_ni,
+          runtime: 10,
+          visitorsPerDay: +entry.sample_size / 10,
+          standardDeviation: +entry.stddev_base,
+          variants: entry.nr_variants - 1,
+          impact: 0,
+          threshold: -entry.effect_size,
+
+          // Non-inferiority specials
+          mu: math.getMuFromRelativeDifference(opts),
+          opts,
+
+          // Configuration
+          isNonInferiority: true,
+          comparisonMode: COMPARISON_MODE.ALL,
+          trafficMode: TRAFFIC_MODE.TOTAL,
+          testType: TEST_TYPE.CONTINUOUS
+        })
+      }
+    })
+    test('Calculating power', () => {
+      const results = cases.map((entry, index) => {
+        const {
+          targetPower,
+          sample,
+          falsePositiveRate,
+          standardDeviation,
+          baseRate,
+          impact,
+          alpha,
+          mu,
+          opts,
+          variants
+        } = entry
+        const approx = power({
+          total_sample_size: sample,
+          base_rate: baseRate,
+          effect_size: 0,
+          alpha: getCorrectedAlpha(falsePositiveRate, variants),
+          variants,
+          sd_rate: standardDeviation,
+          alternative: 'greater',
+          mu,
+          opts,
+        })
+        return executeTest(index, targetPower, approx)
       })
-      return executeTest(index, targetImpact, approx)
+
+      const errors = results.filter(r => /false/.test(r))
+      expect(errors.length).toBe(0)
     })
 
-    const errors = results.filter((r) => /false/.test(r))
-    expect(errors.length).toBe(0)
+    test('Calculating sample', () => {
+      const results = cases.map((entry, index) => {
+        const {
+          targetPower,
+          sample: targetSample,
+          falsePositiveRate,
+          standardDeviation,
+          baseRate,
+          impact,
+          alpha,
+          mu,
+          opts,
+          variants
+        } = entry
 
+        if (targetPower < 0.7 || targetPower >= 0.999) return skipTest(index)
+        const approx = sample({
+          base_rate: baseRate,
+          effect_size: 0,
+          alpha: getCorrectedAlpha(falsePositiveRate, variants),
+          beta: 1 - targetPower,
+          variants,
+          sd_rate: standardDeviation,
+          alternative: 'greater',
+          mu,
+          opts,
+        })
+        return executeTest(index, targetSample, approx)
+      })
+
+      const errors = results.filter(r => /false/.test(r))
+      expect(errors.length).toBe(0)
+    })
+    test('Calculating effect size', () => {
+      const results = cases.map((entry, index) => {
+        const {
+          targetPower,
+          sample,
+          falsePositiveRate,
+          standardDeviation,
+          baseRate,
+          threshold,
+          alpha,
+          mu,
+          opts,
+          variants
+        } = entry
+        if (targetPower < 0.7 || targetPower >= 0.999) return skipTest(index)
+        const approx = impact({
+          total_sample_size: sample,
+          base_rate: baseRate,
+          alpha: getCorrectedAlpha(falsePositiveRate, variants),
+          beta: 1 - targetPower,
+          variants,
+          sd_rate: standardDeviation,
+          alternative: 'greater',
+          mu,
+          opts,
+        })
+        return executeTest(index, threshold, approx)
+      })
+
+      const errors = results.filter(r => /false/.test(r))
+      expect(errors.length).toBe(0)
+    })
   })
 })
